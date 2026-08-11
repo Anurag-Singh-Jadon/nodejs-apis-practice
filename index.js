59ac6aba07d420bc7270b1e00d5c6982e55cbfb1
Anurag Jadon <jadon.anurag2107@gmail.com>
2026-08-11 12:35:29 +0530

    Add separate Html files

--- index.js
+++ index.js
@@ -1,25 +1,43 @@
-import home from './pages/home.js';
-import login from './pages/login.js';
-import submit from './pages/submit.js';
+// import home from './pages/home.js';
+// import login from './pages/login.js';
+// import submit from './pages/submit.js';
+// import express from 'express';
+
+// const app = express();
+
+// app.use(express.urlencoded({ extended: true }));
+
+// app.get('/home', (req, res) => {
+//     res.send(home());
+// });
+
+// app.get('/login', (req, res) => {
+//     res.send(login());
+// });
+
+// app.post('/submit', (req, res) => {
+//     console.log(req.body);
+//     res.send(submit());
+// });
+
+// app.listen(3200, () => {
+//     console.log('Server is running on http://localhost:3200');
+// });
+
 import express from 'express';
+import path from 'path';
+const app = express()
 
-const app = express();
-
-app.use(express.urlencoded({ extended: true }));
-
-app.get('/home', (req, res) => {
-    res.send(home());
-});
-
-app.get('/login', (req, res) => {
-    res.send(login());
-});
-
-app.post('/submit', (req, res) => {
-    console.log(req.body);
-    res.send(submit());
-});
-
-app.listen(3200, () => {
-    console.log('Server is running on http://localhost:3200');
-});
+app.get("/", (req, resp) => {
+    const abPath = path.resolve('view/home.html')
+    resp.sendFile(abPath)
+})
+app.get("/login", (req, resp) => {
+    const abPath = path.resolve('view/login.html')
+    resp.sendFile(abPath)
+})
+app.get("/about", (req, resp) => {
+    const abPath = path.resolve('view/about.html')
+    resp.sendFile(abPath)
+})
+app.listen(3200)
